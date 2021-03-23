166c7f6053b351b8678e626f52ab648b18cf8f92
JingZhao <dreamer132397@gmail.com>
2021-03-23 21:44:28 +1100

    create index page and question page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  resources :questions
+  root 'welcome#index'
+  get 'questions/new'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
