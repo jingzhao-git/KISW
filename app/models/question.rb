4198025a62a50dbe7293cb97259cecdd98b51357
JingZhao <dreamer132397@gmail.com>
2021-03-25 06:48:09 +1100

    add linkes between questions and users

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ApplicationRecord
-
+    belongs_to :user
     validates :title, presence: true, length: { minimum: 6, maximum: 100 }
     validates :text, presence: true, length: { minimum: 10, maximum: 300 }
 end
